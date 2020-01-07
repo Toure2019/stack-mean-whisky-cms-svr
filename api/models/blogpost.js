59999b71b5acd55b16bed763c58aa69116355942
Toure2019 <51404116+Toure2019@users.noreply.github.com>
2020-01-07 10:02:56 +0100

    S2-CRUD: vers une base MongoDB via Mongoose (V10 à V16)

---
+++ api/models/blogpost.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const blogpostSchema = mongoose.Schema({
+    title: String,
+    subTitle: String,
+    image: String,
+    content: String,
+    createdOn: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('BlogPost', blogpostSchema);
--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-const express = require('express');
-const app     = express();
-const api     = require('./api/v1/index');
-const cors    = require('cors');
+const express   = require('express');
+const app       = express();
+const api       = require('./api/v1/index');
+const cors      = require('cors');
+const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose');
+const connection = mongoose.connection;
 
 app.set('port', process.env.PORT || 3000);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Implémentation du CORS: npm install cors (midleware)
 app.use(cors());
@@ -16,8 +23,18 @@ app.use((req, res) => {
     const err = new Error('404 - Not found !');
     err.status = 404;
     res.json({ msg: '404 - Not found !', err: err });
-})
+});
 
-app.listen(app.get('port'), () => {
-    console.log(`Express server listening on ${app.get('port')}`);
+mongoose.connect('mongodb://localhost:27017/whiskycms', { useNewUrlParser: true });
+
+connection.on('error', () => {
+    console.error(`Connection to MongoDB error : ${err.message}`);
+});
+
+connection.once('open', () => {
+    console.log('Connected to MongoDB');
+
+    app.listen(app.get('port'), () => {
+        console.log(`Express server listening on ${app.get('port')}`);
+    });
 });
